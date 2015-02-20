6b2ceaa515de4b0048b465a0984e70ecdd480731
Nickola <nickola@nickola.ru>
2015-02-20 04:17:36 +0300

    Foodcritic fixes

--- metadata.rb
+++ metadata.rb
@@ -2,9 +2,9 @@ name              "ssh-keys"
 maintainer        "Nickolay Kovalev"
 maintainer_email  "nickola@nickola.ru"
 license           "Apache 2.0"
-description       "Creates \"authorized_keys\" in user \"~/.ssh\" directory from a data bag"
+description       "Creates \"authorized_keys\" in user \"~/.ssh\" directory from a data bag (encrypted data bag supported)"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.2.6"
+version           "1.2.7"
 
 %w{ubuntu debian redhat centos fedora freebsd}.each do |os|
   supports os
--- recipes/default.rb
+++ recipes/default.rb
@@ -34,8 +34,12 @@ if node['ssh_keys']
 
       if not bag_users.kind_of?(String) and not bag_users.kind_of?(Array) and bag_users['groups'] != nil
         Array(bag_users['groups']).each do |group_name|
-          search(:users, 'groups:' + group_name) do |user|
-            ssh_keys += Array(user['ssh_keys'])
+          if not Chef::Config[:solo]
+            search(:users, 'groups:' + group_name) do |search_user|
+              ssh_keys += Array(search_user['ssh_keys'])
+            end
+          else
+            Chef::Log.warn("This recipe uses search for users detection by groups. Chef Solo does not support search.")
           end
         end
       end
